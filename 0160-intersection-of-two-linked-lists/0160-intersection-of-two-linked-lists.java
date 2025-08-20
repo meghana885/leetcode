ba08711eea81f27679c3f760f10599a39a059f7b
KODURU MEGHANA <23951a66a1@iare.ac.in>
2025-08-20 10:00:51 +0530

    Time: 1 ms (99.87%), Space: 48.4 MB (80.76%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pt1=headA;
+        ListNode pt2=headB;
+        while(pt1 !=pt2){
+            if(pt1==null){
+                pt1=headB;
+            } else{
+                pt1=pt1.next;
+            }
+            if(pt2==null){
+                pt2=headA;
+            }else{
+                pt2=pt2.next;
+            }
+        }
+        return pt1;
+    }
+}
