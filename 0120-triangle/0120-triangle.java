7fd9fa8d3b23ba521b6a2f2bc5a58f6e4269986f
KODURU MEGHANA <23951a66a1@iare.ac.in>
2025-09-25 13:13:13 +0530

    Time: 3 ms (79.02%), Space: 45.9 MB (12.73%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+
+        // Initialize dp with the last row
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        // Bottom-up DP from second last row to the top
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
+                dp[col] = triangle.get(row).get(col) + Math.min(dp[col], dp[col + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+}
